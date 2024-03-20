0c710f814e6113f998371e94dd85371d30304fc2
Antti Alavilo <A2302144@edu.lapinamk.fi>
2024-03-20 09:14:51 +0200

    initialize create task screen
    title

---
+++ DaWe-Planner/screens/CreateTask.js
@@ -0,0 +1,10 @@
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+
+export default function CreateTask() {
+    return (
+        <View>
+            <Text>Create task screen</Text>
+        </View>
+    )
+}
