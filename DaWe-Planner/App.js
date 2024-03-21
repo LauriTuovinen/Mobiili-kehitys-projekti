4a7b80a5ae6991c3fc242b60aee217b14948a2f4
Retrobitti <PekkaParviainen@protonmail.com>
2024-03-21 13:59:48 +0200

    First version of weekly screen
    title

--- DaWe-Planner/App.js
+++ DaWe-Planner/App.js
@@ -3,6 +3,7 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import * as SQLite from "expo-sqlite"
 import { useState, useEffect } from 'react';
 import Navigation from './components/navigation';
+import WeeklyScreen from './screens/Weekly';
 
  //SQLite should always be used in app.js to avoid any errors
 export default function App() {
@@ -106,6 +107,8 @@ export default function App() {
       {showTasks()}
       <StatusBar style="auto" />
     </View>
+    
+    
   );
 }
 
