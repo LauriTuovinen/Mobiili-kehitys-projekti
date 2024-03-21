2bec9e227d8ea57d03d138fa59a7c55b093787af
Retrobitti <PekkaParviainen@protonmail.com>
2024-03-21 14:15:35 +0200

    Revert "First version of weekly screen"
    This reverts commit 4a7b80a5ae6991c3fc242b60aee217b14948a2f4.

--- DaWe-Planner/App.js
+++ DaWe-Planner/App.js
@@ -3,7 +3,6 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import * as SQLite from "expo-sqlite"
 import { useState, useEffect } from 'react';
 import Navigation from './components/navigation';
-import WeeklyScreen from './screens/Weekly';
 
  //SQLite should always be used in app.js to avoid any errors
 export default function App() {
@@ -107,8 +106,6 @@ export default function App() {
       {showTasks()}
       <StatusBar style="auto" />
     </View>
-    
-    
   );
 }
 
