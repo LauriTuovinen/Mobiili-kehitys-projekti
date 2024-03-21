298d295c8500ee07d3e164ca1eb232f3a79a290a
Antti Alavilo <A2302144@edu.lapinamk.fi>
2024-03-21 15:31:52 +0200

    Revert "Added variables, as well as some packages"
    This reverts commit ba5ed0f7d9574a009210765b2798c48bd1654e6c.

--- DaWe-Planner/screens/CreateTask.js
+++ DaWe-Planner/screens/CreateTask.js
@@ -1,54 +1,10 @@
-import { useState } from 'react';
-import { Button, Text, TextInput, View, Image, Picker } from 'react-native';
-import DatePicker from 'react-native-datepicker'
-import DateTimePicker from 'react-native-ui-datepicker'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 
 export default function CreateTask() {
-    const [taskName, setTaskName] = useState('');
-    const [description, setDescription] = useState('');
-    const [date, setDate] = useState(new Date());
-    const [startTime, setStartTime] = useState(new Date());
-    const [endTime, setEndTime] = useState(new Date());
-    const [repeatInterval, setRepeatInterval] = useState('none');
-    const [image, setImage] = useState(null);
-
-
     return (
         <View>
             <Text>Create task screen</Text>
-            <View>
-                <Text>Create a task:</Text>
-                <TextInput 
-                    placeholder='Enter task title'
-                    value={taskName}
-                    onChangeText={text => setTaskName(text)}
-                />
-                <Text>Info:</Text>
-                <TextInput 
-                    placeholder='Enter Description'
-                    value={description}
-                    onChangeText={text => setDescription(text)}
-                />
-                <Text>Set start and end times:</Text>
-                <DatePicker
-                    date={startTime}
-                    mode="time"
-                    format='HH:mm'
-                    onDateChange={time => setStartTime(time)}
-                    confirmBtnText='Confirm'
-                    cancelBtnText='Cancel'
-                />
-                <DatePicker
-                    date={endTime}
-                    mode="time"
-                    format='HH:mm'
-                    onDateChange={time => setEndTime(time)}
-                    confirmBtnText='Confirm'
-                    cancelBtnText='Cancel'
-                />
-
-            </View>
         </View>
     )
 }
