7f3a15cba1e5c9d250dc851bd9d41898c6e7429a
Retrobitti <PekkaParviainen@protonmail.com>
2024-04-10 12:00:01 +0300

    Initial version of TaskInfo
    TaskInfo takes id and shows the task data for that id. If no id is given the id is currently hardcoded to 6.

--- DaWe-Planner/screens/Settings.js
+++ DaWe-Planner/screens/Settings.js
@@ -4,15 +4,23 @@ import { dropTaskTable } from "../components/database";
 
 const db = database.db;
 
+
 const deleteDatabase = () => {
     dropTaskTable(db);
 }
+
+const getTaskByID = async () => {
+   const task = await database.getTaskbyId(db, 5)
+   console.log("Task by id 5: ", task)
+}
+
 
 export default function Settings(){
     return(
        <View>
         <Text>Settings</Text>
         <Button title="Delete Database" onPress={deleteDatabase}></Button>
+        <Button title="Get by id" onPress={getTaskByID}></Button>
        </View>
     )
 }
