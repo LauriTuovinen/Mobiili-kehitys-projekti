5c982f8bdbd8afd44fbfae5eebd8c8d0739e2272
AAlavilo <121102033+AAlavilo@users.noreply.github.com>
2024-04-10 16:40:02 +0300

    Added adding images
    It works! Believe it Dattebayo!! It stores the data as a file path but hasn't been tested if it retrieves the actual picture or the path as a string. SuS

--- DaWe-Planner/screens/Settings.js
+++ DaWe-Planner/screens/Settings.js
@@ -1,6 +1,6 @@
 import { View, Text, Button } from "react-native";
 import database from "../components/database";
-import { dropTaskTable } from "../components/database";
+import { dropTaskTable, dropTable, createDB } from "../components/database";
 
 const db = database.db;
 
@@ -8,11 +8,18 @@ const deleteDatabase = () => {
     dropTaskTable(db);
 }
 
+const deleteTable = async () => {
+    dropTable(db);
+    createDB(db);
+
+}
+
 export default function Settings(){
     return(
        <View>
         <Text>Settings</Text>
         <Button title="Delete Database" onPress={deleteDatabase}></Button>
+        <Button title="Delete Table" onPress={deleteTable}></Button>
        </View>
     )
 }
